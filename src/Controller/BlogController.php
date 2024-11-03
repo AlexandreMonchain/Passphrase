725aa3639a9028924cfb9bc05bcdc70707ffa20b
AlexandreMonchain <contact@alexandremonchain.fr>
2024-11-03 18:57:53 +0100

    Suppression du markDown

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use League\CommonMark\CommonMarkConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,22 +11,16 @@ class BlogController extends AbstractController
     #[Route('/blog/{slug}', name: 'blog_show')]
     public function show(string $slug): Response
     {
-        // Chemin vers le fichier Markdown
-        $filePath = __DIR__ . '/../../content/blog/' . $slug . '.md';
+        // Chemin vers l'article
+        $filePath = __DIR__ . '/../../content/blog/' . $slug . '.htm';
 
         // Vérifier si le fichier existe
         if (!file_exists($filePath)) {
             throw $this->createNotFoundException('Page not found');
         }
 
-        // Lire le contenu du fichier Markdown
-        $markdownContent = file_get_contents($filePath);
-
-        // Initialiser le convertisseur Markdown -> HTML
-        $converter = new CommonMarkConverter();
-
-        // Convertir le Markdown en HTML
-        $htmlContent = $converter->convert($markdownContent);
+        // Lire le contenu du fichier
+        $htmlContent = file_get_contents($filePath);
 
         // Rendre la page Twig avec le contenu HTML
         return $this->render('blog/show.html.twig', [
